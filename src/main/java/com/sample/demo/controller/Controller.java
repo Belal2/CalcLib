953a4d3ff2fbd7311289e58745f5612198fcb277
Belal2 <belal.ash22@gmail.com>
2024-01-16 22:23:11 +0200

    nnk

--- src/main/java/com/sample/demo/controller/Controller.java
+++ src/main/java/com/sample/demo/controller/Controller.java
@@ -7,14 +7,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.sample.demo.service.CalculateService;
 
+import static java.lang.System.*;
+
 @RestController
 public class Controller {
-	@Autowired
-	CalculateService service;
+
+	private CalculateService service;
 
 	@GetMapping("/avarage/{vals}")
 	public int getAvg(@PathVariable(name = "vals") int[] vals) {
-		System.err.println(vals.length);
+		err.println(vals.length);
 		return service.avg(vals);
+	}
+	@Autowired
+	public void setService(CalculateService service) {
+		this.service = service;
 	}
 }
