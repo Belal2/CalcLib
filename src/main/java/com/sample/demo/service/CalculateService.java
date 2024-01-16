6c39a84110e4f97e1e379a8e46952a37d433640e
Belal2 <belal.ash22@gmail.com>
2024-01-16 22:28:02 +0200

    fdfdfas

--- src/main/java/com/sample/demo/service/CalculateService.java
+++ src/main/java/com/sample/demo/service/CalculateService.java
@@ -2,16 +2,18 @@ package com.sample.demo.service;
 
 import org.springframework.stereotype.Service;
 
+import static java.lang.System.*;
+
 @Service
 public class CalculateService {
 
 	public int avg(int[] args) {
-		System.err.println(args.length);
+		err.println(args.length);
 		int total = 0;
 		for (int i : args) {
 			total += args[i - 1];
 		}
-		System.err.println(total);
+		err.println(total);
 		return total / args.length;
 	}
 }
